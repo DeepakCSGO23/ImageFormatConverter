955ac401ae8ae7969405e55ae2e24b673e324070
sample github <kkvdhoorika@gmail.com>
2024-07-31 16:05:57 +0530

    Convert All & Download All Feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 // src/App.jsx
 
-import React, { useRef, useState } from "react";
+import { useState } from "react";
 import "./App.css";
-
+import JSZip from "jszip";
+import { saveAs } from "file-saver";
+import { useEffect } from "react";
 const App = () => {
   const [images, setImages] = useState([]);
+  const [isAllImagesCanBeDownloaded, setIsAllImagesCanBeDownloaded] =
+    useState(false);
   // Initializing the selected file state & image preview state
   const handleImageFileChange = (e) => {
     const imageFile = e.target.files[0];
@@ -23,10 +27,9 @@ const App = () => {
   };
 
   // Converting any format to Webp format
-  const handleConvertingImage = () => {
-    if (!selectedFile) {
-      return;
-    }
+  const handleConvertingImage = (index) => {
+    console.log("converting all ");
+    console.log(images[index].file);
     const reader = new FileReader();
     reader.onload = (e) => {
       const img = new Image();
@@ -38,28 +41,82 @@ const App = () => {
         ctx.drawImage(img, 0, 0);
         canvas.toBlob((blob) => {
           const url = URL.createObjectURL(blob);
-          // setConvertedFileUrl(url);
+          console.log(url);
+          setImages((prev) =>
+            prev.map((image, i) =>
+              i === index ? { ...image, convertedImageUrl: url } : image
+            )
+          );
         }, "image/webp");
       };
       img.src = e.target.result;
     };
-    reader.readAsDataURL(selectedFile);
+    reader.readAsDataURL(images[index].file);
   };
 
   // For generating Download
-  // const handleImageDownload = () => {
-  //   if (!convertedFileUrl) return;
-  //   const link = document.createElement("a");
-  //   link.href = convertedFileUrl;
-  //   link.download = `${selectedFile.name.split(".")[0]}`;
-  //   document.body.appendChild(link);
-  //   link.click();
-  //   document.body.removeChild(link);
-  // };
+  const handleImageDownload = (index) => {
+    const link = document.createElement("a");
+    link.href = images[index].convertedImageUrl;
+    link.download = `${images[index].file.name.split(".")[0]}`;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  // For generating Download All as Zip
+  const handleDownloadAll = async () => {
+    const zip = new JSZip();
+    const folder = zip.folder("converted_images");
+    for (let i = 0; i < images.length; i++) {
+      if (images[i].convertedImageUrl) {
+        const response = await fetch(images[i].convertedImageUrl);
+        const blog = await response.blob();
+        const imageFileName = `${images[i].file.name.split(".")[0]}`;
+        const allImageFilesTillNow = Object.keys(folder.files);
+        console.log(folder.files);
+        if (
+          allImageFilesTillNow.includes(
+            `converted_images/${imageFileName}.webp`
+          )
+        ) {
+          folder.file(`${imageFileName}1.webp`, blog);
+        } else {
+          folder.file(`${imageFileName}.webp`, blog);
+        }
+      }
+    }
+    const content = await zip.generateAsync({ type: "blob" });
+    saveAs(content, "converted_images.zip");
+  };
+  const handleRemoveImage = (index) => {
+    setImages((prev) => prev.filter((image, i) => i !== index && image));
+  };
+
+  // Converting all images at once
+  const handleConvertingAllImages = () => {
+    images.forEach((_, index) => {
+      handleConvertingImage(index);
+    });
+  };
+
+  // useeffect
+  useEffect(() => {
+    const totalImagesDownloadable = images.filter(
+      (image) => image.convertedImageUrl
+    ).length;
+    if (totalImagesDownloadable === images.length) {
+      console.log("all images can be download");
+      setIsAllImagesCanBeDownloaded(true);
+    } else {
+      console.log("there is some image/images to be converted");
+      setIsAllImagesCanBeDownloaded(false);
+    }
+  }, [images]);
   return (
-    <div className="flex flex-col bg-slate-900 roboto_cregular text-white h-screen w-screen items-center justify-center space-y-20 pl-10 pr-10">
+    <div className="flex flex-col bg-slate-900 roboto_cregular text-white h-screen w-screen items-center justify-center space-y-20">
       {/* All Popups */}
-      <div className="space-y-4">
+      <div className="space-y-4 w-[80%]">
         <h1 className="text-4xl roboto_csbold">
           Image Converter - Any Format to{" "}
           <span className="text-green-300"> Webp</span>
@@ -68,54 +125,91 @@ const App = () => {
           Convert any image to WebP effortlessly. Simple and fast conversion
         </p>
       </div>
-      <input type="file" onChange={handleImageFileChange} />
-      {/* {selectedFile ? (
-        <div className="h-52 p-10 text-xs bg-white flex flex-col items-start w-full space-y-4 rounded-3xl">
-          <div className="flex space-x-2">
-            <img
-              src="/icons/addmore.svg"
-              alt="addmore"
-              height="20"
-              width="20"
-            />
-            <h1 className="text-black">Add More File</h1>
-          </div>
-          <div className="flex items-center space-x-4 border-t-2 pt-4">
-            <div className="flex space-x-1">
+      {images.length > 0 ? (
+        <div className="flex flex-col space-y-6 text-xs bg-white w-[85%] rounded-3xl p-8">
+          <div className="nav flex items-center space-x-1">
+            <label
+              htmlFor="fileupload"
+              className="flex space-x-1 items-center text-[#212427] cursor-pointer"
+            >
               <img
-                src={imagePreviewUrl}
-                alt="test"
-                height="50"
-                width="50"
-                className="h-10 w-10"
+                src="icons/addmore.svg"
+                className="h-5 w-5"
+                alt="addmore"
+                height="45"
+                width="45"
               />
-              <div className="flex flex-col text-xs text-gray-400">
-                <h1>{selectedFile.name}</h1>
-                <span>{(selectedFile.size / 1024).toFixed(2)}kb</span>
+              <span className="flex-shrink-0"> Add More Posts</span>
+            </label>
+            <input
+              id="fileupload"
+              type="file"
+              className="file:hidden w-0 relative right-10"
+              onChange={handleImageFileChange}
+            />
+            {isAllImagesCanBeDownloaded ? (
+              <button
+                onClick={handleDownloadAll}
+                className="bg-[#2BCF6D] p-4 flex-shrink-0 ml-12 rounded-2xl"
+              >
+                Download All
+              </button>
+            ) : (
+              <button
+                onClick={handleConvertingAllImages}
+                className="bg-[#2BCF6D] p-4 flex-shrink-0 ml-12 rounded-2xl"
+              >
+                Convert All
+              </button>
+            )}
+          </div>
+
+          <div className="main flex flex-col items-start justify-between space-y-2 overflow-x-scroll border-t-2 pt-6">
+            {images.map((image, index) => (
+              <div
+                key={index}
+                className="flex space-x-2 items-center justify-evenly"
+              >
+                <img
+                  src={image.previewImageUrl}
+                  className="object-cover h-8 w-8 bg-slate-200"
+                  alt="previewimage"
+                  height="25"
+                  width="25"
+                />
+                <div className="flex flex-col flex-shrink-0 text-gray-400">
+                  <h1>{image.file?.name}</h1>
+                  <h1>{(image.file?.size / 1024).toFixed(2)} kb</h1>
+                </div>
+                <img
+                  onClick={() => handleRemoveImage(index)}
+                  src="/icons/close.svg"
+                  className="h-5 w-5"
+                  alt="remove"
+                  height="25"
+                  width="25"
+                />
+                {image.convertedImageUrl ? (
+                  <button
+                    onClick={() => handleImageDownload(index)}
+                    className="bg-[#2BCF6D] p-4 ml-auto rounded-2xl"
+                  >
+                    Download
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => handleConvertingImage(index)}
+                    className="bg-[#2BCF6D] p-4 ml-auto rounded-2xl flex-shrink-0"
+                  >
+                    Convert to Webp
+                  </button>
+                )}
               </div>
-            </div>
-            <div className="flex">
-              <img src="icons/close.svg" alt="close" height="25" width="25" />{" "}
-              {convertedFileUrl ? (
-                <button
-                  onClick={handleImageDownload}
-                  className="bg-[#2BCF6D] text-white text-opacity-80 hover:text-opacity-100 duration-300 p-4 rounded-3xl"
-                >
-                  Download
-                </button>
-              ) : (
-                <button
-                  onClick={handleConvertingImage}
-                  className="bg-[#2BCF6D] text-white text-opacity-80 hover:text-opacity-100 duration-300 p-4 rounded-3xl"
-                >
-                  Convert to Webp
-                </button>
-              )}
-            </div>
+            ))}
           </div>
         </div>
       ) : (
-        <div className="h-52 flex flex-col items-center justify-center w-full text-xs space-y-4 border-2 border-dashed rounded-3xl">
+        <div className="h-48 flex flex-col items-center justify-center w-[80%] text-xs space-y-4 border-2 border-dashed rounded-3xl">
           <input
             className="file:p-3 file:rounded-3xl w-40"
             type="file"
@@ -124,7 +218,7 @@ const App = () => {
           />
           <h1>or drop your files here</h1>
         </div>
-      )} */}
+      )}
     </div>
   );
 };
